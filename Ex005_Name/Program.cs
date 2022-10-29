b479638dd7a07ca4ed2bb1e4c2a59629f674df64
Shuvchik <ishuvs@gmail.com>
2022-10-29 21:16:12 +0300

    Masha Example

---
+++ Ex005_Name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Как тебя зовут? ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ойёй, тебе особый привет :)");
+}
+else
+{
+    Console.Write("Ну привет, ");
+    Console.WriteLine(username);
+}
